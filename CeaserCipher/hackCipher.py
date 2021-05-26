2c28ef5d4d8fce28cfa1d094070a394781c7a69c
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-26 21:29:16 +0100

    Little variable modification

--- CeaserCipher/hackCipher.py
+++ CeaserCipher/hackCipher.py
@@ -4,7 +4,7 @@
 # Hacking key is 8
 message = "VHFUHW"
 ALPABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-for key in range(len(ALPA)):
+for key in range(len(ALPABET)):
     translated = ''
     for symbol in message:
         if symbol in ALPABET:
