94f101d488f66153bd98f870235cd73eb9941d20
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-26 21:28:11 +0100

    Variable modified

--- CeaserCipher/hackCipher.py
+++ CeaserCipher/hackCipher.py
@@ -11,7 +11,7 @@ for key in range(len(ALPA)):
             num = ALPABET.find(symbol)
             num = num - key
             if num < 0:
-                num = num + len(ALPA)
+                num = num + len(ALPABET)
             translated = translated + ALPA[num]
 
             print('Hacking key #%s: %s' % (key, translated))
