a403c1e50bb9454e36acb7be713f61062719883c
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-06-02 21:42:19 +0100

    Simple Function in PHP

---
+++ PHP/Function.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Writing PHP Function</title>
+</head>
+
+<body>
+    <?php
+ /* Defining a PHP Function */
+ function addFunction($num1, $num2) {
+    $sum = $num1 + $num2;
+    echo "Sum of the two numbers is : $sum";
+    }
+    
+    addFunction(10, 20);
+    
+ /* Calling a PHP Function */
+ //writeMessage();
+ ?>
+</body>
+
+</html>
