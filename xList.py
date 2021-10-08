1e9a2a91b93a7b54b79cb808d0c11d55c38bb7e6
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-10-08 21:42:55 +0100

    Demontration of a list

---
+++ xList.py
@@ -0,0 +1,14 @@
+def xList(listAug):
+    myList = []
+    for items in listAug:
+        if items % 2 == 0:
+            myList.append("black")
+        else:
+           continue            
+    return print(myList) 
+            
+myList = [1,2,3,]
+
+xList(myList)
+
+        
