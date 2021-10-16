9ed667841de99f6097647e84e3bcaf7ed9376be7
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-10-16 23:15:21 +0100

    Dealing with list

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+List= [13,15]
+for i in range(1,10):
+   List.append(f"{i}")
+
+print(List)
--- xList.py
+++ xList.py
@@ -1,3 +1,16 @@
+List= [13,15]
+for i in range(1,10):
+   List.append(f"{i}")
+
+print(List)
+
+#A  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#B  ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+#C  ['{i}', '{i}', '{i}', '{i}', '{i}', '{i}', '{i}', '{i}', '{i}']
+
+
+#girl have skills
+
 def xList(listAug):
     myList = []
     for items in listAug:
@@ -9,6 +22,25 @@ def xList(listAug):
             
 myList = [1,2,3,]
 
-xList(myList)
 
-        
+#xList(myList)
+
+newList = myList.copy() + myList.append(int(1))
+print(newList)
+
+
+intList = [1,5,4,8,12,3,6,65,48,21,321,21,89]
+print(intList)
+print(intList.append(10))
+print(intList)
+intList[0] =  + 3
+print(intList[0] ==  + 3)
+print(intList)
+intList.extend(1000,2000)
+print(intList)
+
+
+
+
+
+
