a67edf792e3fc3f9fc03d27a74bfe444f7dbaa4e
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-11-27 16:51:34 +0100

    All finalies

---
+++ C codes/StudentCatelog.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+	//Declering student structure
+struct student
+	{
+	char name[50];
+	int age;
+	char mat[10];
+	};
+struct student structStudent(void);
+void displayStudentInfos(struct student std);
+
+int main(void)
+{
+int number, i;
+printf("How many students do you wanna enter thier info? ");
+scanf("%d",&number);
+struct student s[number];	
+	
+	//Entring student Infos
+int j,count = 1;
+for (j = 0;j < number;j ++){
+	printf("Student %d\n",count);
+	s[j] = structStudent();
+	count ++;
+}
+//Outputing the student infos
+ count =1;
+ 
+for(i =0;i < number; i++){
+	printf("Student %d\n",count);
+	displayStudentInfos(s[i]);
+	count ++;
+}
+ count =1;
+ 
+};
+
+struct student structStudent(void){
+		struct student std;
+		
+		printf("Enter the name of student: ");
+		scanf("%s",&std.name);
+		
+		printf("Enter the age of student: ");
+		scanf("%d",&std.age);
+		
+		printf("Enter the matricle of student: ");
+		scanf("%s",&std.mat);
+		
+	return std;
+}
+
+void displayStudentInfos(struct student std){
+		printf("{ \n");
+		printf("  Name:  %s\n",std.name);
+		printf("  Age: %d\n",std.age);
+		printf("  Matricle: %s\n",std.mat);
+		printf("} \n\n");
+}
