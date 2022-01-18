068d7f162745b27da3ca124e37854145bd6486fd
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2022-01-18 09:41:01 +0100

    Aaary

--- C codes/MultidimentionalArray.c
+++ C codes/MultidimentionalArray.c
@@ -1,22 +1,60 @@
 #include<stdio.h>
- 
+int n;
 int Find(int x);
+int* FillArray(int arr[n][n]);
 int main(){
 
-    Find(3);
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&n);
+    
+    int arr[n][n];
+    int* x = FillArray(arr[n][n]);
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",x[i][j]);
+        }
+    }
 
 return 0;
-}
+};
+
 int Find(int x){
     int arr[3][3]={{2,14,0},{7,9,12},{8,4,3}};
-for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 3; j++){ 
-        if( x == arr[i][j]){
-            return 1;
-        }
-        else{
-            return 0;
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){ 
+            if( x == arr[i][j]){
+                printf("True");
             }
+            else if(arr[i][j]==arr[2][2]){
+                printf("False");
+                }
+        }
     }
-return 10;
-};
+}
+
+int* FillArray(int arr[n][n]){
+    int k=1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("Enter elements %d: \n",k);
+            scanf("%d",&arr[i][j]);
+            k++;
+        }
+    }
+    return arr;
+}
+
+/* int DisplayArray(int* x){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d",x[i][j])
+        }
+}  */
