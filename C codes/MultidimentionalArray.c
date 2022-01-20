81ee3e743898566d7f0817930003fb6ff73e1fc6
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2022-01-20 07:06:38 +0100

    Binary search on multidimentional array

---
+++ C codes/MultidimentionalArray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ 
+int Find(int x);
+int main(){
+
+    Find(3);
+
+return 0;
+}
+int Find(int x){
+    int arr[3][3]={{2,14,0},{7,9,12},{8,4,3}};
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){ 
+        if( x == arr[i][j]){
+            return 1;
+        }
+        else{
+            return 0;
+            }
+    }
+return 10;
+};
--- Python/test.py
+++ Python/test.py
@@ -1,14 +1,15 @@
 
 def test(n,j):
+    j=j+1
     if n == 1:
-        print(n)
-        j=j+1
+      
+        print(n,j)
+        
     else:
-        for i in range(n*n):
+        for i in range(pow(2,n)):
+            print(">",i)
             test(n-1,j)
-    print("J:",j)
-    
-    
+   ## print("J:",j)
 j = 0
 num = int(input("Enter the value of n: "))
 test(num,j)
